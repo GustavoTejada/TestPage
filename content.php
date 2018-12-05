904ccb6f8185c1a39d13c9bf7900277860b8d408
Gustavo <tejadagustavo20@gmail.com>
2018-12-05 10:18:58 -0300

    ejemplo require funcionando

---
+++ content.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Content</title>
+	<?php require 'moduls/links.php'?>
+</head>
+<body>
+	<?php require 'moduls/navbar.php'?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<!-- links -->
+	<?php require  'moduls/links.php'?>
+</head>
+<body>
+	<header>
+		<!-- NavBar -->
+		<?php require 'moduls/navbar.php'?>
+	</header>
+
+	<div class="container fluid">
+
+		<div class="jumbotron">
+			<h1>Pagina de prueba</h1>
+			<p>...</p>
+			<p><a class="btn btn-primary btn-lg" href="content.php" role="button">Ver Más</a></p>
+		</div>
+
+	</div>
+</body>
+</html>

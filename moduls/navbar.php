cb3bba4cfc53e77fc3d6766a6a974effed77dda6
Gustavo <tejadagustavo20@gmail.com>
2018-12-05 12:28:32 -0300

    navbar

--- moduls/navbar.php
+++ moduls/navbar.php
@@ -14,7 +14,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a ui-sref="content">Content</a></li>
+        <li ui-sref-active="active"><a ui-sref="content">Content</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu">
